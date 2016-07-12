6c6a8d396f6d4cebef1bfc2903c9772dcc00a47f
Jacob Goodpasture <jrgoodpasture@gmail.com>
2016-07-12 13:03:31 +0200

    Recreated LoginActivity (Added logo/title and made content_login_screen) There is still a problem with setContent in both activities that has to with how everything is packaged.

---
+++ app/src/main/java/com/jacobgoodpasture/magellan/LoginScreenActivity.java
@@ -0,0 +1,25 @@
+package com.jacobgoodpasture.magellan;
+//package project.github.cinemasnob.view;
+
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.R;
+//import project.github.cinemasnob.R;
+
+
+public class LoginScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+
+    }
+
+}
