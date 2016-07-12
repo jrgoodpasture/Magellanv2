55b6850b0b43a8058f544be35b1f3d4c45b1bdfc
Jacob Goodpasture <jrgoodpasture@gmail.com>
2016-07-12 22:38:47 +0200

    Got rid of some errors

--- app/src/main/java/com/jacobgoodpasture/magellan/LoginScreenActivity.java
+++ app/src/main/java/com/jacobgoodpasture/magellan/LoginScreenActivity.java
@@ -1,14 +1,11 @@
 package com.jacobgoodpasture.magellan;
-//package project.github.cinemasnob.view;
-
-
+//package project.github.Magellanv2;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.R;
 //import project.github.cinemasnob.R;
 
 
